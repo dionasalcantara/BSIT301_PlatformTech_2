5f76fdab085f81010b2d262247c25761d03f1127
dionasalcantara <a.dionas@gmail.com>
2019-09-17 12:58:37 +0800

    First commit by Dionas

---
+++ Dionas_Alcantara/lotto.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+
+
+const int lotteryDigits = 55;
+const int SIZE = 6;
+
+
+
+int generateLottery(int[], int, int);
+int userInput(int[], int);
+int matchCounter(int[], int[], int);
+void displayNumbers(int[], int[]);
+void winnerOrLoser(int);
+
+
+
+using namespace std;
+
+int main()
+{
+	
+	int lottery[6] = {0, 0, 0, 0, 0, 0};
+	int user[6] = {0, 0, 0, 0, 0, 0};
+	int matches = 0;
+
+	
+	generateLottery(lottery, SIZE, lotteryDigits);
+
+	userInput(user, SIZE);
+
+	matchCounter(lottery, user, matches);
+
+	displayNumbers(lottery, user);
+
+	winnerOrLoser(matches);
+
+	system("pause");
+	return 0;
+}
+
+int generateLottery(int lottery[], int, int)
+{
+	unsigned seed = time(0);
+	srand(seed);
+
+	for (int y=0; y<SIZE; y++)
+	{
+		lottery[y] = rand() % lotteryDigits;
+	}
+	
+	return lottery[0], lottery[1], lottery[2], lottery[3], lottery[4], lottery [5];
+}
+
+int userInput(int user[], int)
+{	
+	cout << "This program will simulate a lottery.\n\n";
+
+	for (int y=0; y<SIZE; y++)
+	{
+		cout << "Enter a digit between 0 and 55:---> ";
+		cin >> user[y];
+
+		while (user[y]<0 || user[y]>55)
+		{
+		cout << "Error! Entry must be between 0 and 55:---> ";
+		cin >> user[y];
+		}
+	}
+
+	return user[0], user[1], user[2], user[3], user[4], user[5];
+}
+
+int matchCounter(int lotto[], int input[], int)
+{
+	int match = 0;
+
+	for (int x = 0; x < SIZE; ++x)
+	{
+		if (lotto[x] == input[x])
+			match = match + 1;
+	}
+
+	return match;
+}
+
+void displayNumbers(int lottery[], int user[])
+{
+	cout << "\nThe winning lottery numbers are: " << lottery[0] << " " << lottery[1] << " " << lottery[2] << " " << lottery[3] << " " << lottery[4] << " " << lottery[5] << endl;
+	cout << "Your lottery numbers are: " << user[0] << " " << user[1] << " " << user[2] << " " << user[3] << " " << user[4] << " " << user[5] << endl;
+}
+
+
+
+void winnerOrLoser(int matches)
+{
+	cout << "You matched " << matches << " numbers";
+
+	if (matches != SIZE)
+		cout << "\nSorry--YOU LOSE. Better luck next time.\n";
+	else
+		cout << "\nCongratulations--YOU WIN!!!!\n";
+		
+}
+
